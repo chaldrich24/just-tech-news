d5f079412841c7086502038b83905b588781e1cc
Snake <chaldrich24@gmail.com>
2021-11-27 16:33:16 -0500

    add helpers

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
 const path = require('path');
+
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
